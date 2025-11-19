1dedc7626ff64f3e0c35212869e36f074be84127
Liamell Cruz Disla <liamell_cruz@ucne.edu.do>
2025-11-19 15:05:15 -0400

    login screen

---
+++ app/src/main/java/edu/ucne/whatabook/presentation/screen/LoginScreen.kt
@@ -0,0 +1,95 @@
+package edu.ucne.whatabook.presentation.screen
+
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import edu.ucne.whatabook.presentation.login.LoginEvent
+import edu.ucne.whatabook.presentation.login.LoginViewModel
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun LoginScreen(
+    viewModel: LoginViewModel = hiltViewModel(),
+    onLoginSuccess: () -> Unit,
+    onOpenRegister: () -> Unit
+) {
+    val state by viewModel.uiState.collectAsState()
+
+    LaunchedEffect(state.usuario) {
+        if (state.usuario != null) onLoginSuccess()
+    }
+
+    Scaffold { innerPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+                .padding(24.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+            Text(
+                "Iniciar sesión",
+                style = MaterialTheme.typography.headlineMedium
+            )
+
+            Spacer(modifier = Modifier.height(24.dp))
+
+            // Usuario
+            OutlinedTextField(
+                value = state.userName,
+                onValueChange = { viewModel.onEvent(LoginEvent.UserNameChanged(it)) },
+                label = { Text("Usuario") },
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            Spacer(modifier = Modifier.height(12.dp))
+
+            // Contraseña
+            OutlinedTextField(
+                value = state.password,
+                onValueChange = { viewModel.onEvent(LoginEvent.PasswordChanged(it)) },
+                label = { Text("Contraseña") },
+                modifier = Modifier.fillMaxWidth(),
+                keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
+                visualTransformation = PasswordVisualTransformation()
+            )
+
+            Spacer(modifier = Modifier.height(24.dp))
+
+
+            Button(
+                onClick = { viewModel.onEvent(LoginEvent.SubmitLogin) },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(50.dp),
+                colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF702222))
+            ) {
+                Text("Iniciar sesión", color = Color.White)
+            }
+
+            Spacer(modifier = Modifier.height(12.dp))
+
+
+            TextButton(onClick = onOpenRegister) {
+                Text("Crear cuenta")
+            }
+
+
+            state.error?.let {
+                Spacer(modifier = Modifier.height(12.dp))
+                Text(it, color = MaterialTheme.colorScheme.error)
+            }
+        }
+    }
+}
