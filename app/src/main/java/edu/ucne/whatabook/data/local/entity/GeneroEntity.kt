131530bed44217572d755474cf42432a1cccd7ac
Liamell Cruz Disla <liamell_cruz@ucne.edu.do>
2025-11-26 20:33:30 -0400

    genero entity

---
+++ app/src/main/java/edu/ucne/whatabook/data/local/entity/GeneroEntity.kt
@@ -0,0 +1,11 @@
+package edu.ucne.whatabook.data.local.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "generos")
+data class GeneroEntity(
+    @PrimaryKey(autoGenerate = true)
+    val generoId: Int = 0,
+    val nombre: String = ""
+)
