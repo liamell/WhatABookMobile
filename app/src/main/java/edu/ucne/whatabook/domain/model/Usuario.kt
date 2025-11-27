64e6a75be6bdcc28b21b17688dc43f7972dd470f
Liamell Cruz Disla <liamell_cruz@ucne.edu.do>
2025-11-26 20:23:25 -0400

    modelo de Usuario

---
+++ app/src/main/java/edu/ucne/whatabook/domain/model/Usuario.kt
@@ -0,0 +1,9 @@
+package edu.ucne.whatabook.domain.model
+
+
+data class Usuario(
+    val usuarioId: Int = 0,
+    val nombre: String = "",
+    val correo: String = "",
+    val password: String = ""
+)
