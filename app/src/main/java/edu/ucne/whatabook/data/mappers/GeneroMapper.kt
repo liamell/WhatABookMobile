6666eff5a3b65b3d027cb9197f561239feb4bb82
Liamell Cruz Disla <liamell_cruz@ucne.edu.do>
2025-11-26 20:33:45 -0400

    genero mapper

---
+++ app/src/main/java/edu/ucne/whatabook/data/mappers/GeneroMapper.kt
@@ -0,0 +1,14 @@
+package edu.ucne.whatabook.data.mappers
+
+import edu.ucne.whatabook.data.local.entity.GeneroEntity
+import edu.ucne.whatabook.domain.model.Genero
+
+fun GeneroEntity.toDomain() = Genero(
+    generoId = generoId,
+    nombre = nombre
+)
+
+fun Genero.toEntity() = GeneroEntity(
+    generoId = generoId,
+    nombre = nombre
+)
