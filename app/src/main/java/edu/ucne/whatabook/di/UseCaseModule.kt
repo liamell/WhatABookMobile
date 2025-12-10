f553fdf53636aadef00f2b75ab3ffaa9fd97ffa2
Liamell Cruz Disla <liamell_cruz@ucne.edu.do>
2025-12-10 11:41:31 -0400

    feature: agrega los usecases de login, register, resgistrar compra, historial de compra

--- app/src/main/java/edu/ucne/whatabook/di/UseCaseModule.kt
+++ app/src/main/java/edu/ucne/whatabook/di/UseCaseModule.kt
@@ -5,7 +5,13 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import edu.ucne.whatabook.domain.repository.CompraRepository
 import edu.ucne.whatabook.domain.repository.LibroRepository
+import edu.ucne.whatabook.domain.repository.UsuarioRepository
+import edu.ucne.whatabook.domain.usecase.compra.GetHistorialComprasUseCase
+import edu.ucne.whatabook.domain.usecase.compra.RegistrarCompraUseCase
+import edu.ucne.whatabook.domain.usecase.user.LoginUseCase
+import edu.ucne.whatabook.domain.usecase.user.RegisterUserUseCase
 import edu.ucne.whatabook.domain.usecase.libros.GetLibrosUseCase
 import edu.ucne.whatabook.domain.usecase.libros.SearchLibrosUseCase
 import edu.ucne.whatabook.domain.usecase.libros.GetLibroByIdUseCase
@@ -39,4 +45,26 @@ object UseCaseModule {
     fun provideGetLibrosByGeneroUseCase(
         repository: LibroRepository
     ): GetLibrosByGeneroUseCase = GetLibrosByGeneroUseCase(repository)
+
+    @Provides
+    @Singleton
+    fun provideLoginUseCase(repository: UsuarioRepository) =
+        LoginUseCase(repository)
+
+    @Provides
+    @Singleton
+    fun provideRegisterUserUseCase(repository: UsuarioRepository) =
+        RegisterUserUseCase(repository)
+
+    @Provides
+    @Singleton
+    fun provideRegistrarCompraUseCase(repository: CompraRepository) =
+        RegistrarCompraUseCase(repository)
+
+    @Provides
+    @Singleton
+    fun provideGetHistorialComprasUseCase(repository: CompraRepository) =
+        GetHistorialComprasUseCase(repository)
+
 }
+
