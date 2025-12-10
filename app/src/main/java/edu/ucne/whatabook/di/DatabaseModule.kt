aa46a37c1ecb449046f847288360002582b0caea
Liamell Cruz Disla <liamell_cruz@ucne.edu.do>
2025-12-10 10:40:56 -0400

    fix: importar apiservice

--- app/src/main/java/edu/ucne/whatabook/di/DatabaseModule.kt
+++ app/src/main/java/edu/ucne/whatabook/di/DatabaseModule.kt
@@ -8,10 +8,18 @@ import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import edu.ucne.whatabook.data.local.AppDatabase
+import edu.ucne.whatabook.data.local.DatabaseSeeder
 import edu.ucne.whatabook.data.local.dao.CarritoDao
+import edu.ucne.whatabook.data.local.dao.CompraDao
 import edu.ucne.whatabook.data.local.dao.GeneroDao
 import edu.ucne.whatabook.data.local.dao.LibroDao
 import edu.ucne.whatabook.data.local.dao.UsuarioDao
+import edu.ucne.whatabook.data.remote.WhatABookApiService
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Provider
 import javax.inject.Singleton
 
 @Module
@@ -20,25 +28,58 @@ object DatabaseModule {
 
     @Provides
     @Singleton
+    fun provideApplicationScope() = CoroutineScope(SupervisorJob())
+
+    @Provides
+    @Singleton
     fun provideDatabase(
-        @ApplicationContext context: Context
+        @ApplicationContext context: Context,
+        generoDaoProvider: Provider<GeneroDao>,
+        scope: CoroutineScope
     ): AppDatabase {
+
+        val seeder = DatabaseSeeder(
+            scope,
+            generoDaoProvider
+        )
+
         return Room.databaseBuilder(
             context,
             AppDatabase::class.java,
             "whatabook.db"
-        ).build()
+        )
+            .fallbackToDestructiveMigration()
+            .addCallback(seeder)
+            .build()
     }
 
     @Provides
+    @Singleton
     fun provideLibroDao(db: AppDatabase): LibroDao = db.libroDao()
 
     @Provides
+    @Singleton
+    fun provideGeneroDao(db: AppDatabase): GeneroDao = db.generoDao()
+
+    @Provides
+    @Singleton
     fun provideUsuarioDao(db: AppDatabase): UsuarioDao = db.usuarioDao()
 
     @Provides
+    @Singleton
     fun provideCarritoDao(db: AppDatabase): CarritoDao = db.carritoDao()
 
     @Provides
-    fun provideGeneroDao(db: AppDatabase): GeneroDao = db.generoDao()
+    @Singleton
+    fun provideCompraDao(db: AppDatabase): CompraDao = db.compraDao()
+
+    @Provides
+    @Singleton
+    fun provideApiService(): WhatABookApiService {
+        return Retrofit.Builder()
+            .baseUrl("http://WhatABook.somee.com/api/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(WhatABookApiService::class.java)
+    }
 }
