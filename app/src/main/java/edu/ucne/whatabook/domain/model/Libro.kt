fb674533726f23cac719d81717022a4c6845f322
Liamell Cruz Disla <liamell_cruz@ucne.edu.do>
2025-12-10 05:25:39 -0400

    reafactor: añadir campo gantidad

--- app/src/main/java/edu/ucne/whatabook/domain/model/Libro.kt
+++ app/src/main/java/edu/ucne/whatabook/domain/model/Libro.kt
@@ -4,9 +4,11 @@ package edu.ucne.whatabook.domain.model
 data class Libro(
     val libroId: Int = 0,
     val titulo: String = "",
-    val autor: String = "",
-    val precio: Double = 0.0,
+    val autores: String = "",
     val descripcion: String = "",
+    val precio: Double = 0.0,
     val imagenUrl: String = "",
-    val generoId: Int = 0
+    val generoId: Int,
+    val cantidad: Int
+
 )
