844f1fba835cf429600e8dd18d99198fcd63cc8e
Liamell Cruz Disla <liamell_cruz@ucne.edu.do>
2025-12-10 05:30:17 -0400

    refactor: mejora del main

--- app/src/main/java/edu/ucne/whatabook/MainActivity.kt
+++ app/src/main/java/edu/ucne/whatabook/MainActivity.kt
@@ -4,10 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
-import edu.ucne.whatabook.ui.home.HomeViewModel
-import edu.ucne.whatabook.ui.screens.HomeScreen
+import edu.ucne.whatabook.navigation.AppNavHost // Importación clave
 import edu.ucne.whatabook.ui.theme.WhatABookTheme
 
 @AndroidEntryPoint
@@ -15,20 +14,14 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContent {
-
-            val homeViewModel: HomeViewModel = viewModel()
-
             WhatABookTheme {
 
-                HomeScreen(
-                    uiState = homeViewModel.uiState,
-                    onSearch = homeViewModel::search,
-                    onLibroClick = {},
-                    onCarritoClick = {},
-                    onPerfilClick = {},
-                    onFiltrarGenero = homeViewModel::filtrarPorGenero
-                )
+                val navController = rememberNavController()
+
+
+                AppNavHost(navController = navController)
             }
         }
     }
