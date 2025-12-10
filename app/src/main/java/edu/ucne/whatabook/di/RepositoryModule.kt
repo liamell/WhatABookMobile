3fe0436dd5682a4ff4b1080d97a4c0a6f3ab0173
Liamell Cruz Disla <liamell_cruz@ucne.edu.do>
2025-12-10 11:38:48 -0400

    feature: agregar provide compra, usuarios, carrito, genero provide al repositorymodule

--- app/src/main/java/edu/ucne/whatabook/di/RepositoryModule.kt
+++ app/src/main/java/edu/ucne/whatabook/di/RepositoryModule.kt
@@ -1,13 +1,23 @@
 package edu.ucne.whatabook.di
 
-
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import edu.ucne.whatabook.data.local.dao.LibroDao
+import edu.ucne.whatabook.data.local.AppDatabase
+import edu.ucne.whatabook.data.local.dao.*
+import edu.ucne.whatabook.data.local.datastore.SessionManager
+import edu.ucne.whatabook.data.remote.WhatABookApiService
+import edu.ucne.whatabook.data.repositoryimpl.CarritoRepositoryImpl
+import edu.ucne.whatabook.data.repositoryimpl.CompraRepositoryImpl
+import edu.ucne.whatabook.data.repositoryimpl.GeneroRepositoryImpl
 import edu.ucne.whatabook.data.repositoryimpl.LibroRepositoryImpl
+import edu.ucne.whatabook.data.repositoryimpl.UsuarioRepositoryImpl
+import edu.ucne.whatabook.domain.repository.CarritoRepository
+import edu.ucne.whatabook.domain.repository.CompraRepository
+import edu.ucne.whatabook.domain.repository.GeneroRepository
 import edu.ucne.whatabook.domain.repository.LibroRepository
+import edu.ucne.whatabook.domain.repository.UsuarioRepository
 import javax.inject.Singleton
 
 @Module
@@ -17,6 +27,48 @@ object RepositoryModule {
     @Provides
     @Singleton
     fun provideLibroRepository(
-        dao: LibroDao
-    ): LibroRepository = LibroRepositoryImpl(dao)
+        libroDao: LibroDao,
+        apiService: WhatABookApiService
+    ): LibroRepository = LibroRepositoryImpl(
+        libroDao,
+        apiService
+    )
+
+    @Provides
+    @Singleton
+    fun provideGeneroRepository(
+        generoDao: GeneroDao,
+        apiService: WhatABookApiService
+    ): GeneroRepository = GeneroRepositoryImpl(
+        generoDao,
+        apiService
+    )
+
+    @Provides
+    @Singleton
+    fun provideCarritoRepository(
+        carritoDao: CarritoDao,
+        libroDao: LibroDao,
+        database: AppDatabase
+    ): CarritoRepository = CarritoRepositoryImpl(
+        carritoDao,
+        libroDao,
+        database
+    )
+
+    @Provides
+    @Singleton
+    fun provideUsuarioRepository(
+        usuarioDao: UsuarioDao,
+        sessionManager: SessionManager
+    ): UsuarioRepository = UsuarioRepositoryImpl(
+        usuarioDao,
+        sessionManager
+    )
+
+    @Provides
+    @Singleton
+    fun provideCompraRepository(
+        impl: CompraRepositoryImpl
+    ): CompraRepository = impl
 }
