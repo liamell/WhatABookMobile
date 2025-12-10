7c94b9d5d17c59efd7b52a4d18ed285833fb5a73
Liamell Cruz Disla <liamell_cruz@ucne.edu.do>
2025-12-10 10:05:06 -0400

    reafactor userid

--- app/src/main/java/edu/ucne/whatabook/data/local/entity/CarritoEntity.kt
+++ app/src/main/java/edu/ucne/whatabook/data/local/entity/CarritoEntity.kt
@@ -1,5 +1,4 @@
 package edu.ucne.whatabook.data.local.entity
-
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
@@ -8,8 +7,9 @@ data class CarritoEntity(
     @PrimaryKey(autoGenerate = true)
     val itemId: Int = 0,
     val libroId: Int = 0,
+    val userId: Int,
     val titulo: String = "",
     val precio: Double = 0.0,
     val cantidad: Int = 1,
-    val imagenUrl: String = ""
+    val imagenUrl: String = "",
 )
