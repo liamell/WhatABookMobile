e66fa097344d9cdc60632e113805ed1cdd3d5aff
Liamell Cruz Disla <liamell_cruz@ucne.edu.do>
2025-12-10 09:44:41 -0400

    reafactor llama a apiservice

--- app/src/main/java/edu/ucne/whatabook/data/repositoryimpl/GeneroRepositoryImpl.kt
+++ app/src/main/java/edu/ucne/whatabook/data/repositoryimpl/GeneroRepositoryImpl.kt
@@ -1,16 +1,18 @@
 package edu.ucne.whatabook.data.repositoryimpl
 
-
 import edu.ucne.whatabook.data.local.dao.GeneroDao
 import edu.ucne.whatabook.data.mappers.toDomain
 import edu.ucne.whatabook.data.mappers.toEntity
+import edu.ucne.whatabook.data.remote.WhatABookApiService
 import edu.ucne.whatabook.domain.model.Genero
 import edu.ucne.whatabook.domain.repository.GeneroRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
+import javax.inject.Inject
 
-class GeneroRepositoryImpl(
-    private val generoDao: GeneroDao
+class GeneroRepositoryImpl @Inject constructor(
+    private val generoDao: GeneroDao,
+    private val apiService: WhatABookApiService
 ) : GeneroRepository {
 
     override fun getGeneros(): Flow<List<Genero>> {
@@ -20,6 +22,12 @@ class GeneroRepositoryImpl(
     }
 
     override suspend fun syncGeneros() {
-        // Aquí puedes simular API
+        try {
+            val generosRemotos = apiService.getGeneros()
+            val generosEntity = generosRemotos.map { it.toEntity() }
+            generoDao.insertAll(generosEntity)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
     }
 }
