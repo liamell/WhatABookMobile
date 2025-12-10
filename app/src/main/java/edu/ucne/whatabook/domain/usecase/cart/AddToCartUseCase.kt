cc1e955679b82e859ed620de347d77a06274d8fb
Liamell Cruz Disla <liamell_cruz@ucne.edu.do>
2025-12-10 10:01:07 -0400

    feature agregar cantidad,

--- app/src/main/java/edu/ucne/whatabook/domain/usecase/cart/AddToCartUseCase.kt
+++ app/src/main/java/edu/ucne/whatabook/domain/usecase/cart/AddToCartUseCase.kt
@@ -1,12 +1,29 @@
 package edu.ucne.whatabook.domain.usecase.cart
-
 import edu.ucne.whatabook.domain.model.CarritoItem
 import edu.ucne.whatabook.domain.repository.CarritoRepository
+import edu.ucne.whatabook.domain.repository.LibroRepository
+import javax.inject.Inject
 
-class AddToCartUseCase(
-    private val repository: CarritoRepository
+
+class AddToCartUseCase @Inject constructor(
+    private val repository: CarritoRepository,
+    private val libroRepository: LibroRepository
 ) {
-    suspend operator fun invoke(item: CarritoItem) {
-        repository.addItem(item)
+    suspend operator fun invoke(newItem: CarritoItem) {
+
+        val cantidadAAgregar = 1
+
+        val existingItem = repository.getCartItemByLibroId(
+            libroId = newItem.libroId,
+            userId = newItem.userId
+        )
+
+
+        repository.performAddToCartTransaction(
+            libroId = newItem.libroId,
+            cantidadAAgregar = cantidadAAgregar,
+            existingItem = existingItem,
+            newItem = newItem
+        )
     }
 }
