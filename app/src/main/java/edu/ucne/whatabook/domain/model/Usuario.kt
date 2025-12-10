e87f23e23d2617645f02eabd5541385a5f9fd4d3
Liamell Cruz Disla <liamell_cruz@ucne.edu.do>
2025-12-10 09:55:52 -0400

    feature foto de perfil

--- app/src/main/java/edu/ucne/whatabook/domain/model/Usuario.kt
+++ app/src/main/java/edu/ucne/whatabook/domain/model/Usuario.kt
@@ -5,5 +5,6 @@ data class Usuario(
     val usuarioId: Int = 0,
     val nombre: String = "",
     val correo: String = "",
-    val password: String = ""
+    val password: String = "",
+    val fotoPerfilUri: String? = null
 )
