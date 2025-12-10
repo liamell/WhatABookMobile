26300e55164f1b64a7d0d423f4715a43568f0c93
Liamell Cruz Disla <liamell_cruz@ucne.edu.do>
2025-12-10 09:50:19 -0400

    reafactor cambia a flow

--- app/src/main/java/edu/ucne/whatabook/domain/usecase/cart/GetCartItemsUseCase.kt
+++ app/src/main/java/edu/ucne/whatabook/domain/usecase/cart/GetCartItemsUseCase.kt
@@ -1,9 +1,14 @@
 package edu.ucne.whatabook.domain.usecase.cart
 
+import edu.ucne.whatabook.domain.model.CarritoItem
 import edu.ucne.whatabook.domain.repository.CarritoRepository
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
 
-class GetCartItemsUseCase(
+class GetCartItemsUseCase @Inject constructor(
     private val repository: CarritoRepository
 ) {
-    suspend operator fun invoke() = repository.getCarrito()
+    operator fun invoke(userId: Int): Flow<List<CarritoItem>> {
+        return repository.getCarrito(userId)
+    }
 }
