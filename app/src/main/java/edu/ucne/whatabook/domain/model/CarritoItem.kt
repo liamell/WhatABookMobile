5b6506461dbd0e8b6066133047b4468e96a3f81b
Liamell Cruz Disla <liamell_cruz@ucne.edu.do>
2025-12-10 10:05:58 -0400

    feature carritoitem

--- app/src/main/java/edu/ucne/whatabook/domain/model/CarritoItem.kt
+++ app/src/main/java/edu/ucne/whatabook/domain/model/CarritoItem.kt
@@ -4,6 +4,7 @@ package edu.ucne.whatabook.domain.model
 data class CarritoItem(
     val itemId: Int = 0,
     val libroId: Int = 0,
+    val userId: Int,
     val titulo: String = "",
     val precio: Double = 0.0,
     val cantidad: Int = 1,
