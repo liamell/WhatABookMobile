fe78d74aa134d33fd244f92cd14725b688ced504
Liamell Cruz Disla <liamell_cruz@ucne.edu.do>
2025-12-10 11:05:39 -0400

    refactore: agrega crear libro

--- app/src/main/java/edu/ucne/whatabook/presentation/libros/LibroEvent.kt
+++ app/src/main/java/edu/ucne/whatabook/presentation/libros/LibroEvent.kt
@@ -1,8 +1,10 @@
-package edu.ucne.whatabook.ui.libros
+package edu.ucne.whatabook.presentation.libros
+import edu.ucne.whatabook.domain.model.Libro
 
 sealed class LibroEvent {
     object LoadLibros : LibroEvent()
     data class Buscar(val query: String) : LibroEvent()
     data class FiltrarPorGenero(val generoId: Int) : LibroEvent()
     data class VerDetalle(val libroId: Int) : LibroEvent()
+    data class CrearLibro(val libro: Libro) : LibroEvent()
 }
