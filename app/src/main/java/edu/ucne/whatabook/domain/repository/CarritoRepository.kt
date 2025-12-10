1402dce3df6e66d3848e2a54095d74457db1de51
Liamell Cruz Disla <liamell_cruz@ucne.edu.do>
2025-12-10 10:08:03 -0400

    feature: agrega getcarritoitem, getcarditembyid

--- app/src/main/java/edu/ucne/whatabook/domain/repository/CarritoRepository.kt
+++ app/src/main/java/edu/ucne/whatabook/domain/repository/CarritoRepository.kt
@@ -1,18 +1,31 @@
 package edu.ucne.whatabook.domain.repository
 
-
 import edu.ucne.whatabook.domain.model.CarritoItem
 import kotlinx.coroutines.flow.Flow
 
 interface CarritoRepository {
 
-    fun getCarrito(): Flow<List<CarritoItem>>
+    suspend fun increaseItemQuantity(item: CarritoItem)
+    suspend fun decreaseItemQuantity(item: CarritoItem)
+
+    fun getCarrito(userId: Int): Flow<List<CarritoItem>>
 
     suspend fun addItem(item: CarritoItem)
 
-    suspend fun updateItem(item: CarritoItem)
+    suspend fun getCartItemByLibroId(libroId: Int, userId: Int): CarritoItem?
 
     suspend fun removeItem(id: Int)
 
-    suspend fun clearCarrito()
+    suspend fun clearCart(userId: Int)
+
+    suspend fun updateItem(item: CarritoItem)
+
+    suspend fun getCartItemById(itemId: Int): CarritoItem?
+
+    suspend fun performAddToCartTransaction(
+        libroId: Int,
+        cantidadAAgregar: Int,
+        existingItem: CarritoItem?,
+        newItem: CarritoItem
+    )
 }
