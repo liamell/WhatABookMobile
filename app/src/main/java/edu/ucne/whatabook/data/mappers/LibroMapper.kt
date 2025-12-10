0d647cff72f7757601b38cf330bc552ceba9d941
Liamell Cruz Disla <liamell_cruz@ucne.edu.do>
2025-12-10 11:08:50 -0400

    refactore: cambiar de autor a autores para evitar incompatinilidad

--- app/src/main/java/edu/ucne/whatabook/data/mappers/LibroMapper.kt
+++ app/src/main/java/edu/ucne/whatabook/data/mappers/LibroMapper.kt
@@ -1,24 +1,68 @@
 package edu.ucne.whatabook.data.mappers
 
 import edu.ucne.whatabook.data.local.entity.LibroEntity
+import edu.ucne.whatabook.data.remote.dto.LibroDto
 import edu.ucne.whatabook.domain.model.Libro
 
 fun LibroEntity.toDomain() = Libro(
     libroId = libroId,
     titulo = titulo,
-    autor = autor,
-    precio = precio,
+    autores = autores,
     descripcion = descripcion,
-    imagenUrl = imagenUrl,
-    generoId = generoId
+    precio = precio,
+    imagenUrl = imagenUrl ?: "",
+    generoId = generoId,
+    cantidad = cantidad,
 )
 
-fun Libro.toEntity() = LibroEntity(
-    libroId = libroId,
-    titulo = titulo,
-    autor = autor,
-    precio = precio,
-    descripcion = descripcion,
-    imagenUrl = imagenUrl,
-    generoId = generoId
-)
+fun Libro.toEntity(): LibroEntity {
+    return LibroEntity(
+        libroId = this.libroId,
+        titulo = this.titulo,
+        autores = this.autores,
+        descripcion = this.descripcion,
+        precio = this.precio,
+        imagenUrl = this.imagenUrl,
+        generoId = this.generoId,
+        cantidad = this.cantidad
+    )
+}
+
+fun LibroDto.toEntity(): LibroEntity {
+    return LibroEntity(
+        libroId = this.libroId ?: 0,
+        titulo = this.titulo ?: "Sin Título",
+        autores = this.autor ?: "Desconocido",
+        descripcion = this.descripcion ?: "",
+        precio = this.precio ?: 0.0,
+        imagenUrl = this.imagenUrl ?: "",
+        cantidad = this.cantidad ?: 0,
+        generoId = this.generoId ?: 0
+    )
+}
+
+fun LibroDto.toDomain(): Libro {
+    return Libro(
+        libroId = this.libroId ?: 0,
+        titulo = this.titulo ?: "Sin Título",
+        autores = this.autor ?: "Desconocido",
+        descripcion = this.descripcion ?: "",
+        precio = this.precio ?: 0.0,
+        imagenUrl = this.imagenUrl ?: "",
+        cantidad = this.cantidad ?: 0,
+        generoId = this.generoId ?: 0
+    )
+}
+
+fun Libro.toDto(): LibroDto {
+    return LibroDto(
+        libroId = this.libroId,
+        titulo = this.titulo,
+        autor = this.autores,
+        descripcion = this.descripcion,
+        precio = this.precio,
+        imagenUrl = this.imagenUrl,
+        cantidad = this.cantidad,
+        generoId = this.generoId
+    )
+}
