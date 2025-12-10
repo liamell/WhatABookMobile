7ce77cfe7597abcf4e2b0e2bb366c54a608c47c1
Liamell Cruz Disla <liamell_cruz@ucne.edu.do>
2025-12-10 09:59:46 -0400

    feature getusersseion

--- app/src/main/java/edu/ucne/whatabook/domain/repository/UsuarioRepository.kt
+++ app/src/main/java/edu/ucne/whatabook/domain/repository/UsuarioRepository.kt
@@ -1,6 +1,6 @@
 package edu.ucne.whatabook.domain.repository
-
 import edu.ucne.whatabook.domain.model.Usuario
+import kotlinx.coroutines.flow.Flow
 
 interface UsuarioRepository {
 
@@ -9,7 +9,16 @@ interface UsuarioRepository {
     suspend fun register(usuario: Usuario)
 
     suspend fun getUsuarioById(id: Int): Usuario?
+    fun getCurrentUserId(): Flow<Int>
 
+    fun getUserSessionId(): Flow<Int>
+    fun getUserSessionName(): Flow<String?>
+    fun getUserSessionEmail(): Flow<String?>
 
+    suspend fun logout()
+
+    suspend fun updateFotoPerfil(userId: Int, uri: String)
+
+    suspend fun syncUsuarios()
 
 }
